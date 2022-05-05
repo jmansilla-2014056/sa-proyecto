cbcff4406bf015a1fb087285c2eaba54ba5bb09e
Yaiza Pineda <2993547680101@ingenieria.usac.edu.gt>
2022-05-05 10:43:19 -0600

    cambios ruta

--- Backend/cliente/index.js
+++ Backend/cliente/index.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const context = '/esb/client';
+const context = '/esb/customer';
 const router = require('./rutas/router');
 
 app.use(cors());
